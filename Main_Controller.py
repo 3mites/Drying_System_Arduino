d764b10510e7e45fb46a11a673c8f05eb0f1e252
Victor Anthon Cabang <victorcabang6@gmail.com>
2025-06-09 12:37:00 +0800

    Add files via upload

--- Main_Controller.py
+++ Main_Controller.py
@@ -63,11 +63,11 @@ class SecondWindow(QtWidgets.QMainWindow):
 
     @QtCore.pyqtSlot(str, str, str, str, str)
     def update_temperature_labels(self, t1, t2, t3, t4, t_ave_first):
-        self.ui.label.setText(f"{t1} Â°C")
-        self.ui.label_6.setText(f"{t2} Â°C")
-        self.ui.label_12.setText(f"{t3} Â°C")
-        self.ui.label_11.setText(f"{t4} Â°C")
-        self.ui.label_8.setText(f"Average: {t_ave_first} Â°C")
+        self.ui.label.setText(f"{t1} Ã‚Â°C")
+        self.ui.label_6.setText(f"{t2} Ã‚Â°C")
+        self.ui.label_12.setText(f"{t3} Ã‚Â°C")
+        self.ui.label_11.setText(f"{t4} Ã‚Â°C")
+        self.ui.label_8.setText(f"Average: {t_ave_first} Ã‚Â°C")
 
 
 class FirstWindow(QtWidgets.QMainWindow):
@@ -75,6 +75,9 @@ class FirstWindow(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = Ui_FirstWindow()
         self.ui.setupUi(self)
+        self.fuzzy_timer = QtCore.QTimer(self)
+        self.fuzzy_timer.timeout.connect(self.run_fuzzy_controller)
+        self.fuzzy_timer.start(300000)  #5mins
 
         self.last_valid_drying_seconds = None
 
@@ -94,12 +97,23 @@ class FirstWindow(QtWidgets.QMainWindow):
         self.serial_thread.daemon = True
         self.serial_thread.start()
 
+    def run_fuzzy_controller(self):
+        try:
+            print("Running TemperatureFuzzyController...")
+            fuzzy_control = TemperatureFuzzyController()
+            adjustment = fuzzy_control.temperature_adjustment(self.t_ave_first,self.h_ave)
+            if hasattr(self, 'ser') and self.ser.is_open:
+                message = f"ADJ:{adjustment}\n"
+                self.ser.write(message.encode())
+                print("Sent to Arduino:", message)
+        except Exception as e:
+            print("Error running TemperatureFuzzyController:", e)
     def read_serial_data(self):
         try:
-            ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+            self.ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
             buffer = ""
             while True:
-                line = ser.readline().decode(errors='ignore').strip()
+                line = self.ser.readline().decode(errors='ignore').strip()
                 if not line:
                     continue
 
@@ -109,32 +123,33 @@ class FirstWindow(QtWidgets.QMainWindow):
                     parts = buffer.strip().split()
                     buffer = ""
 
-                    if len(parts) < 15:
+                    if len(parts) < 16:
                         continue
 
                     try:
-                        t1 = parts[0].split("T1:")[1]
-                        t2 = parts[1].split("T2:")[1]
-                        t3 = parts[2].split("T3:")[1]
-                        t4 = parts[3].split("T4:")[1]
-                        t5 = parts[4].split("T5:")[1]
-                        t6 = parts[5].split("T6:")[1]
-                        t7 = parts[6].split("T7:")[1]
-                        t8 = parts[7].split("T8:")[1]
-                        h1 = parts[8].split("H1:")[1]
-                        h2 = parts[9].split("H2:")[1]
-                        t_ave_first = parts[10].split("t_ave_first:")[1]
-                        t_ave_2nd = parts[11].split("t_ave_2nd:")[1]
-                        h_ave = parts[12].split("h_ave:")[1]
-                        pwm_1 = parts[13].split("pwm_1:")[1]
-                        pwm_2 = parts[14].split("pwm_2:")[1]
+                        t0 = parts[0].split("T0:")[1]
+                        t1 = parts[1].split("T1:")[1]
+                        t2 = parts[2].split("T2:")[1]
+                        t3 = parts[3].split("T3:")[1]
+                        t4 = parts[4].split("T4:")[1]
+                        t5 = parts[5].split("T5:")[1]
+                        t6 = parts[6].split("T6:")[1]
+                        t7 = parts[7].split("T7:")[1]
+                        t8 = parts[8].split("T8:")[1]
+                        h1 = parts[9].split("H1:")[1]
+                        h2 = parts[10].split("H2:")[1]
+                        self.t_ave_first = parts[11].split("t_ave_first:")[1]
+                        t_ave_2nd = parts[12].split("t_ave_2nd:")[1]
+                        self.h_ave = parts[13].split("h_ave:")[1]
+                        pwm_1 = parts[14].split("pwm_1:")[1]
+                        pwm_2 = parts[15].split("pwm_2:")[1]
 
                         QtCore.QMetaObject.invokeMethod(
                             self,
                             "update_labels",
                             QtCore.Qt.QueuedConnection,
                             QtCore.Q_ARG(str, t_ave_2nd),
-                            QtCore.Q_ARG(str, h_ave),
+                            QtCore.Q_ARG(str, self.h_ave),
                             QtCore.Q_ARG(str, pwm_2),
                             QtCore.Q_ARG(str, pwm_1)
                         )
@@ -148,7 +163,7 @@ class FirstWindow(QtWidgets.QMainWindow):
                                 QtCore.Q_ARG(str, t2),
                                 QtCore.Q_ARG(str, t3),
                                 QtCore.Q_ARG(str, t4),
-                                QtCore.Q_ARG(str, t_ave_first)
+                                QtCore.Q_ARG(str, self.t_ave_first)
                             )
 
                         if hasattr(self, 'temp_drying_window') and self.temp_drying_window.isVisible():
@@ -170,13 +185,14 @@ class FirstWindow(QtWidgets.QMainWindow):
                                 QtCore.Qt.QueuedConnection,
                                 QtCore.Q_ARG(str, h1),
                                 QtCore.Q_ARG(str, h2),
-                                QtCore.Q_ARG(str, h_ave)
+                                QtCore.Q_ARG(str, self.h_ave)
                             )
 
                         # Save to Excel
                         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                         self.data_log.append({
                             "Timestamp": timestamp,
+                            "T0": t0,
                             "T1": t1,
                             "T2": t2,
                             "T3": t3,
@@ -187,9 +203,9 @@ class FirstWindow(QtWidgets.QMainWindow):
                             "T8": t8,
                             "H1": h1,
                             "H2": h2,
-                            "T_Ave_First": t_ave_first,
+                            "T_Ave_First": self.t_ave_first,
                             "T_Ave_Second": t_ave_2nd,
-                            "H_Ave": h_ave,
+                            "H_Ave": self.h_ave,
                             "PWM_1": pwm_1,
                             "PWM_2": pwm_2
                         })
@@ -207,7 +223,7 @@ class FirstWindow(QtWidgets.QMainWindow):
 
     @QtCore.pyqtSlot(str, str, str, str)
     def update_labels(self, t_ave_2nd, h_ave, pwm_2, pwm_1):
-        self.ui.label.setText(f"{t_ave_2nd} Â°C")
+        self.ui.label.setText(f"{t_ave_2nd} Ã‚Â°C")
         self.ui.label_6.setText(f"{h_ave} %")
         self.ui.label_12.setText(f"{pwm_2}")
         self.ui.label_11.setText(f"{pwm_1}")
@@ -242,7 +258,6 @@ class TempDryingWindow(QtWidgets.QMainWindow):
         self.ui = Ui_TempDryingWindow()
         self.ui.setupUi(self)
         self.first_window = first_window
-        self.centralWidget().layout().setAlignment(QtCore.Qt.AlignTop | QtCore.Qt.AlignLeft)
         self.ui.pushButton.setEnabled(True)
         self.ui.pushButton_2.setEnabled(True)
 
@@ -263,11 +278,11 @@ class TempDryingWindow(QtWidgets.QMainWindow):
 
     @QtCore.pyqtSlot(str, str, str, str, str)
     def update_temperature_labels(self, t5, t6, t7, t8, t_ave_2nd):
-        self.ui.label.setText(f"{t5} Â°C")
-        self.ui.label_6.setText(f"{t6} Â°C")
-        self.ui.label_12.setText(f"{t7} Â°C")
-        self.ui.label_11.setText(f"{t8} Â°C")
-        self.ui.label_8.setText(f"Average: {t_ave_2nd} Â°C")
+        self.ui.label.setText(f"{t5} Ã‚Â°C")
+        self.ui.label_6.setText(f"{t6} Ã‚Â°C")
+        self.ui.label_12.setText(f"{t7} Ã‚Â°C")
+        self.ui.label_11.setText(f"{t8} Ã‚Â°C")
+        self.ui.label_8.setText(f"Average: {t_ave_2nd} Ã‚Â°C")
 
 
 app = QtWidgets.QApplication(sys.argv)
